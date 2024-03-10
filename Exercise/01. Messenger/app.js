02e3cb33459a679b9f2187e08c67ee8966fe918b
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-03-10 20:19:17 +0200

    Create app.js

---
+++ Exercise/01. Messenger/app.js
@@ -0,0 +1,34 @@
+function attachEvents() {
+    document.getElementById("refresh").addEventListener("click", onLoadMsg);
+    document.getElementById("submit").addEventListener("click", onSubmit);
+    const url = "http://localhost:3030/jsonstore/messenger"
+    
+    async function onSubmit(e) {
+        let nameRef = document.querySelector("input[name='author']");
+        let textRef = document.querySelector("input[name='content']"); 
+        let name = nameRef.value;
+        let text = textRef.value;
+        let data = {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({author: name, content: text})
+        }
+        await fetch(url, data);  
+        nameRef.value = "";
+        textRef.value = "";
+    }
+    async function onLoadMsg() {
+        let textAreaRef = document.getElementById("messages");
+        textAreaRef.value = "";
+        let response = await fetch(url); 
+        let data = await response.json(); 
+        Object.values(data).forEach(record => {  
+            textAreaRef.value += `${record.author}: ${record.content}\n`
+        })
+        textAreaRef.value = textAreaRef.value.trim(); 
+    }
+}
+
+attachEvents();
